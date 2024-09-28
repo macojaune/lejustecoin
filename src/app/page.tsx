eb42cba21f9207c2ca62e44df0a7fdb714ee62f3
MarvinL.com <contact@marvinl.com>
2024-09-28 14:33:57 -0400

    chore: SEO content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 import Quiz from "@/components/quiz";
-
+import Script from "next/script";
 export default function Home() {
+  const ldJson = {
+    "@context": "https://schema.org",
+    "@type": "Game",
+    name: "LeJusteCoin",
+    interactionType: "SingleplayerGame",
+  };
+
   return (
     <div className="grid relative items-center min-h-screen bg-orange-01 p-8 pb-20 gap-8 sm:gap-16 sm:p-20 ">
+      <Script type="application/ld+json" id="game-ld-json">
+        {JSON.stringify(ldJson)}
+      </Script>
       <h1 className="text-3xl text-orange-02 self-start text-center sm:text-6xl sm:text-left z-20 font-display">
         LeJusteCoin <small className="text-sm text-zinc-600">Guadeloupe</small>
       </h1>
